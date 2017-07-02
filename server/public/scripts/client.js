3d4c1a835c5c65287a04c3bca4421a85dcbdb147
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-02 13:43:30 -0500

    Client to server communication established. Result computed and displayed.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,27 +1,43 @@
 console.log("JS up and Atom!");
+var mathData = {};
 
 $(document).ready(function(){
 
 // Get input from the user
   // Receive numbers from the HTML inputs
-  // Receive mathmatical operator from the corresponding button
-  // Build an object to transmit data
+  $('button').on('click', function(){
+    var num1 = $('#number1').val();
+    var num2 = $('#number2').val();
+    console.log("Numbers entered: " + num1 + ", " + num2);
 
-// Send input to the server -- AJAX post request
-$.ajax({
-  type: 'POST',
-  url: '/math',
-  data: {},
-  success: function(response){
-    console.log("Hi there");
-  }
-})
-;
+    // Receive mathmatical operator from the corresponding button
+    var operator = $(this).text().toLowerCase();
+    console.log("Math operator: " + operator);
 
-// After successful AJAX response
-  // Receive the result from the server
+    // Build an object to transmit data
+    mathData = {
+      number1: num1,
+      number2: num2,
+      type: operator
+    };
+    console.log(mathData);
 
-  // Display the result for the user
+    // Send input to the server -- AJAX post request
+    $.ajax({
+      type: 'POST',
+      url: '/calculate',
+      data: mathData,
+      success: function(response){
+        console.log("Result received.");
+        // Successful AJAX response:
+        // Receive the result from the server
+        var result = response.answer;
+        console.log(result);
+        // Display the result for the user
+        $('#result').text(result);
+      }
+    });
+  });
 
 // Clear the inputs & reset the calculator
 
--- server/server.js
+++ server/server.js
@@ -6,10 +6,50 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Receive data from the client
+app.post("/calculate", function(req, res){
+  // Retrieve data from client and set up variables
+  var mathData = req.body;
+    console.log("Server has received: " + mathData);
+  var num1 = parseInt(mathData.number1);
+  var num2 = parseInt(mathData.number2);
+  var operator = mathData.type;
+  var result;
+  console.log(num1 + num2 + operator);
+  // Do the Math
+  switch (operator) {
+    case 'add':
+      result = num1 + num2;
+      console.log("The result is: " + result);
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      console.log("The result is: " + result);
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      console.log("The result is: " + result);
+      break;
+    case 'divide':
+      result = num1 / num2;
+      console.log("The result is: " + result);
+      break;
+    default:
+      console.log("Oops. What was that again?");
+  }
+  // Send result back to client
+  theResult = {answer: result};
+  res.send(theResult);
+});
 
-// Do the Math
+
 
 // Catch-all for requests
-
+app.get("/*", function(req, res){
+  var file = req.params[0] || "views/index.html";
+  res.sendFile(path.join(__dirname, "public", file));
+});
 
 // Start up the server
+app.listen(port, function(){
+  console.log("Server up and running on port " + port);
+});
